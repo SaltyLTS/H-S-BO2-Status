2b011be180a7c4055a4dee139b9b13f8d4bed522
Salty <52759884+Salty-Scripts@users.noreply.github.com>
2023-12-08 00:50:04 +0100

    Core panel

--- index.js
+++ index.js
@@ -1,144 +1,165 @@
-const {Client, IntentsBitField, EmbedBuilder} = require('discord.js');
+const Discord = require('discord.js');
 const axios = require('axios');
-const dotenv = require('dotenv')
-const client = new Client({
+const dotenv = require('dotenv');
+const client = new Discord.Client({
 	intents: [
-		IntentsBitField.Flags.Guilds,
-		IntentsBitField.Flags.GuildMembers,
-		IntentsBitField.Flags.GuildMessages,
-		IntentsBitField.Flags.MessageContent,
-	],
+		Discord.IntentsBitField.Flags.Guilds,
+		Discord.IntentsBitField.Flags.GuildMembers,
+		Discord.IntentsBitField.Flags.GuildMessages,
+		Discord.IntentsBitField.Flags.MessageContent
+	]
 });
 
+let Informations = [];
 dotenv.config();
 
-let lastMessage = null;
+const HS_Servers_ID = [
+    {'id': 'Ze2k00zuoT', 'name': 'Tranzit 4', 'category': '1', 'channelId': '1182461833584582726'},
+    {'id': 'L-Gn_5UmcN', 'name': 'Nuketown 4', 'category': '1', 'channelId': '1182461834645749791'},
+    {'id': 'wNIDX6UOmq', 'name': 'Die Rise 4', 'category': '1', 'channelId': '1182461838143795342'},
+    {'id': 'IpXeJzAkqh', 'name': 'Alcatraz 4', 'category': '1', 'channelId': '1182461836709339188'},
+    {'id': 'eD1wSa-xa0', 'name': 'Buried 4', 'category': '1', 'channelId': '1182461839632760893'},
+    {'id': 'TUFCjLFXiO', 'name': 'Origins 4', 'category': '1', 'channelId': '1182461841528586340'},
+
+    {'id': 'Ym376GPxM_', 'name': 'Tranzit 8', 'category': '2', 'channelId': '1182461848273047682'},
+    {'id': 'uYT5u-gScj', 'name': 'Nuketown 8', 'category': '2', 'channelId': '1182461843143413780'},
+    {'id': 'O4rUpUmC2Q', 'name': 'Die Rise 8', 'category': '2', 'channelId': '1182461845706113034'},
+    {'id': 'ThEqoddfGB', 'name': 'Alcatraz 8', 'category': '2', 'channelId': '1182461844405878885'},
+    {'id': 'yh3HkAoge5', 'name': 'Buried 8', 'category': '2', 'channelId': '1182461846993776680'},
+    {'id': 'VJqsVtMQQd', 'name': 'Origins 8', 'category': '2', 'channelId': '1182461849501958236'},
+
+    {'id': '3ftDorwZnO', 'name': 'Alcatraz 8 (2)', 'category': '3', 'channelId': '1182461852324745226'},
+    {'id': 'Qpvi1pc6im', 'name': 'Buried 8 (2)', 'category': '3', 'channelId': '1182461850898681986'},
+    {'id': '1q-5OfQDil', 'name': 'S & S', 'category': '3', 'channelId': '1182419563288199301'}, 
+    {'id': 'QF9MI15KBh', 'name': 'TDM', 'category': '3', 'channelId': '1182419561992179772'}, 
+    {'id': 'zOYxWhkP1L', 'name': 'FFA', 'category': '3', 'channelId': '1182419560637411490'}, 
+    {'id': 'cdJ_-P2opw', 'name': 'GunGame', 'category': '3', 'channelId': '1182419559383306411'} 
+];
+
+function getCategoryById(id) {
+    let _id = HS_Servers_ID.find((server) => server.id === id);
+
+    return _id ? _id.category : "Unknown";
+}
+
+function getServerNameById(id) {
+    let _id = HS_Servers_ID.find((server) => server.id === id);
+
+    return _id ? _id.name : "Unknown";
+}
+
+function getChannelIdById(id) {
+    let _id = HS_Servers_ID.find((server) => server.id === id);
+
+    return _id ? _id.channelId : "Unknown";
+}
+
+async function getInformations(id) {
+    const res = await axios.get(`https://list.plutools.pw/s/${id}/json`);
+
+    let players = res.data['players'];
+    let online  = res.data['online'];
+
+    if (!online) {
+        Informations.push( {'value': `🔴 [${getServerNameById(id)}](https://discord.com/channels/946185722698797108/${getChannelIdById(id)})\n`, 'category': getCategoryById(id), 'online': false, 'players': 0} );
+    } else {
+        if (players.length > 0) {
+            Informations.push( {'value': `🟢 [${getServerNameById(id)}](https://discord.com/channels/946185722698797108/${getChannelIdById(id)})\n`, 'category': getCategoryById(id), 'online': true, 'players': players.length} );
+        } else {
+            Informations.push( {'value': `🟡 [${getServerNameById(id)}](https://discord.com/channels/946185722698797108/${getChannelIdById(id)})\n`, 'category': getCategoryById(id), 'online': true, 'players': 0} );
+        };
+    };
+
+    return;
+};
 
 client.on('ready', () => {
 	console.log(`Bot prêt. [PTERO] \nConnecté en tant que ${client.user.tag}`);
-	setInterval(() => {
-		refreshEmbed();
-	}, 10000);
+    setInterval(() => {
+        let _COne   = "";
+        let _CTwo   = "";
+        let _CThree = "";
+
+        let OnlineServers        = 0;
+        let TotalPlayers         = 0;
+        let MaxPlayers           = 160; 
+        let BestPlayers          = 0;
+        let BestPlayersTimestamp = new Date(Date.now()).toUTCString();
+
+        let waitInformations = new Promise((resolve, reject) => {
+            HS_Servers_ID.forEach((server, index, array) => {
+                getInformations(server.id)
+                if (index === array.length - 1) resolve();
+            });
+        });
+        
+        waitInformations.then(() => {
+            let waitParsing = new Promise((resolve, reject) => {
+                Informations.forEach((serverInfo, index, array) => {
+                    if(serverInfo.online) {
+                        OnlineServers++;
+                    };
+
+                    TotalPlayers += serverInfo.players;
+
+                    if(TotalPlayers > BestPlayers) {
+                        BestPlayers          = TotalPlayers;
+                        BestPlayersTimestamp = new Date(Date.now()).toUTCString();
+                    };
+
+                    switch(serverInfo.category) {
+                        case "1":
+                            _COne += serverInfo.value;
+                            break;
+                        case "2":
+                            _CTwo += serverInfo.value;
+                            break;
+                        case "3":
+                            _CThree += serverInfo.value;
+                            break;
+                    };
+
+                    if (index === array.length - 1) resolve();
+                });   
+            });
+
+            waitParsing.then(() => {
+                Informations = [];
+
+                let statusEmbed = new Discord.EmbedBuilder()
+                    .setTitle("Honte-Servers BO2 Status List")
+                    .setURL("https://honte-servers.com/")
+                    .setDescription(`**Servers are restarting at the end of each game**\n\nServers Online: \`${OnlineServers}\`\nPlayers Online: \`${TotalPlayers}/${MaxPlayers}\`\nPlayers Record: \`${BestPlayers}/${MaxPlayers}\`\nAt \`${BestPlayersTimestamp}\`\n\n**============================================**\n\`\`\`Click a server link to see its status\`\`\``)
+                    .addFields(
+                        {
+                            name: "🟢 : ON",
+                            value: _COne ? _COne : "Empty.",
+                            inline: true
+                        },
+                        {
+                            name: "🟡 : RESTART",
+                            value: _CTwo ? _CTwo : "Empty.",
+                            inline: true
+                        },
+                        {
+                            name: "🔴 : OFF",
+                            value: _CThree ? _CThree : "Empty.",
+                            inline: true
+                        },
+                    )
+                    .setImage("https://cdn.discordapp.com/attachments/982743704865488896/1175294915605954570/d57x7mr-21299832-c9a6-49e0-b37a-6a76d8c7596a.png")
+                    .setThumbnail("https://cdn.discordapp.com/attachments/982743704865488896/1175226523150712832/Logo_Honte.png?ex=656a75fd&is=655800fd&hm=f6c16ad63f3ed573df8a1af933b1a2ecc5c10806253655d69a85cebba9c06c76&")
+                    .setFooter({
+                        text: "Honte-Servers BO2 Bot",
+                        iconURL: "https://cdn.discordapp.com/attachments/982743704865488896/1175226523150712832/Logo_Honte.png?ex=656a75fd&is=655800fd&hm=f6c16ad63f3ed573df8a1af933b1a2ecc5c10806253655d69a85cebba9c06c76&",
+                    }).setTimestamp();
+
+                    client.channels.cache.get('953470259040514178').messages.fetch('1182467864322646060').then(message => {
+                        message.edit({ embeds: [ statusEmbed ] });
+                    });
+            });
+        });
+    }, 180000);
 });
-
-async function refreshEmbed() {
-	try {
-		const response = await axios.get('https://list.plutools.pw/s/o8bl0DA1rF/json');
-		const {
-			ip,
-			port,
-			hostname,
-			players,
-			maxplayers,
-			round,
-			online
-		} = response.data;
-		const Round = round === -1 ? 0 : round;
-		let Ping = '0';
-
-		if (!online) {
-			const embedOffline = new EmbedBuilder()
-				.setTitle("Honte-Servers BO2 Status List")
-  .setURL("https://example.com")
-  .setDescription("**Servers are restarting at the end of each game**\nServers Online: `${serversonline}`\nPlayers Online: `${playersonline}/${maxplayers}`\nPlayers Record: `${bestplayers}/${maxplayers}` at `${timestamp}`\n**============================================**\n```Click a server link to see its status```")
-  .addFields(
-    {
-      name: "🟢 : Online",
-      value: "🟢 [Tranzit 4](https://canary.discord.com)\n🔴 [Nuketown 4](https://canary.discord.com)\n🔴 [Die Rise 4](https://canary.discord.com)\n🔴 [Alcatraz 4](https://canary.discord.com)\n🔴 [Buried 4](https://canary.discord.com)\n🔴 [Origins 4](https://canary.discord.com)",
-      inline: true
-    },
-    {
-      name: "🟡 : Restart",
-      value: "🔴 [Tranzit 8](https://canary.discord.com)\n🔴 [Nuketown 8](https://canary.discord.com)\n🔴 [Die Rise 8](https://canary.discord.com)\n🔴 [Alcatraz 8](https://canary.discord.com)\n🔴 [Buried 8](https://canary.discord.com)\n🔴 [Origins 8](https://canary.discord.com)",
-      inline: true
-    },
-    {
-      name: "🔴 : Offline",
-      value: "🔴 [Alcatraz 8 (2)](https://canary.discord.com)\n🔴 [Buried 8 (2)](https://canary.discord.com)\n🔴 [S & S](https://canary.discord.com)\n🔴 [TDM](https://canary.discord.com)\n🔴 [FFA](https://canary.discord.com)\n🔴 [GunGame](https://canary.discord.com)",
-      inline: true
-    },
-  )
-  .setImage("https://cdn.discordapp.com/attachments/982743704865488896/1175294915605954570/d57x7mr-21299832-c9a6-49e0-b37a-6a76d8c7596a.png")
-  .setThumbnail("https://cdn.discordapp.com/attachments/982743704865488896/1175226523150712832/Logo_Honte.png?ex=656a75fd&is=655800fd&hm=f6c16ad63f3ed573df8a1af933b1a2ecc5c10806253655d69a85cebba9c06c76&")
-  .setFooter({
-    text: "Honte-Servers BO2 Bot",
-    iconURL: "https://cdn.discordapp.com/attachments/982743704865488896/1175226523150712832/Logo_Honte.png?ex=656a75fd&is=655800fd&hm=f6c16ad63f3ed573df8a1af933b1a2ecc5c10806253655d69a85cebba9c06c76&",
-  })
-  .setTimestamp();
-
-
-			const guild = client.guilds.cache.get('946185722698797108');
-			const textChannel = guild.channels.cache.get('1175252347946016788');
-
-			if (lastMessage) {
-				await lastMessage.edit({
-					embeds: [embedOffline]
-				});
-			} else {
-				const sentMessage = await textChannel.send({
-					embeds: [embedOffline]
-				});
-				lastMessage = sentMessage;
-			}
-			return;
-		}
-
-		if (players.length > 0) {
-			averagePing = players.reduce((sum, player) => sum + player.ping, 0) / players.length;
-			averagePing = averagePing.toFixed(2);
-		}
-
-		const embed = new EmbedBuilder()
-			.setColor('#57F287')
-			.setTitle(`${hostname}`)
-			.addFields({
-				name: 'IP',
-				value: `\`${ip}:${port}\``,
-				inline: true
-			}, {
-				name: 'Current server status:',
-				value: `\`🟢 ONLINE\``,
-				inline: true
-			}, {
-				name: '‎ ',
-				value: `‎ `,
-				inline: true
-			}, {
-				name: 'Players',
-				value: `\`${players.length}/${maxplayers.toString()}\``,
-				inline: true
-			}, {
-				name: 'Round',
-				value: `\`${Round.toString()}\``,
-				inline: true
-			}, {
-				name: 'Av. Ping',
-				value: `\`${Ping}\``,
-				inline: true
-			})
-			.setImage('https://media.discordapp.net/attachments/982743704865488896/1175235298704248892/Tranzit.png')
-			.setFooter({
-				text: 'Honte-Servers BO2 Bot',
-				iconURL: 'https://cdn.discordapp.com/attachments/982743704865488896/1175226523150712832/Logo_Honte.png',
-			})
-			.setTimestamp();
-
-		const guild = client.guilds.cache.get('946185722698797108');
-		const textChannel = guild.channels.cache.get('1175252347946016788');
-
-		if (lastMessage) {
-			await lastMessage.edit({
-				embeds: [embed]
-			});
-		} else {
-			const sentMessage = await textChannel.send({
-				embeds: [embed]
-			});
-			lastMessage = sentMessage;
-		}
-	} catch (error) {
-		console.error('Error during refresh:', error);
-	}
-}
 
 client.login(process.env.TOKEN);
